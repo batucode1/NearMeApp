d21f5db19b99fa3a75602b5e26199be9f3dcae91
Batuhan Koç <batuhnkc52@gmail.com>
2025-11-27 13:16:03 +0300

    Initial Commit

---
+++ NearMeApp/NearMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeAppApp.swift
+//  NearMeApp
+//
+//  Created by Batuhan Koç on 27.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NearMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NearMeAppTests/NearMeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeAppTests.swift
+//  NearMeAppTests
+//
+//  Created by Batuhan Koç on 27.11.2025.
+//
+
+import Testing
+@testable import NearMeApp
+
+struct NearMeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
